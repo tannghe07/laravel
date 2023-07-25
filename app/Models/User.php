514305256d009ae8b65c0f039301a56a6e6a75fa
tannghe07 <tannghe07@gmail.com>
2023-07-25 18:55:51 +0700

    create relationship in models

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
