66cd0348530e804f97dfb22202a91f8e62d603b7
tannghe07 <tannghe07@gmail.com>
2023-07-25 19:19:39 +0700

    seeder, factory, faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('123456'),
+            'is_admin' => false,
+            'is_active' => 1,
             'remember_token' => Str::random(10),
         ];
     }
