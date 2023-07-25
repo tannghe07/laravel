6fea22384be0eab5ccb5860fe0fff5c0890d9f7a
tannghe07 <tannghe07@gmail.com>
2023-07-25 19:25:55 +0700

    create route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//user route
+Route::resource('user', UserController::class);
+
+//post route
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [\App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [\App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{id}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{id}', [\App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts', [\App\Http\Controllers\PostController::class, 'destroy'])->name('posts.delete');
