2ab0471de9b5cc3294fa1f7b95b46492970d44fe
tannghe07 <tannghe07@gmail.com>
2023-07-31 16:51:54 +0700

    authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//user route
-Route::middleware('checkadmin')->resource('user', UserController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//post route
-Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [\App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [\App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
-Route::get('/posts/{id}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [\App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts', [\App\Http\Controllers\PostController::class, 'destroy'])->name('posts.delete');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
